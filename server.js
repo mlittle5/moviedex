10cbf349e4272c4dc2b11eac18a56d37119bf1eb
Morgan Little <mrgnlit@gmail.com>
2020-12-07 14:13:02 -0800

    started process for deploying

--- server.js
+++ server.js
@@ -1,29 +1,42 @@
 const express = require("express");
 const morgan = require("morgan");
 const movies = require("./movies.json");
-const cors = require('cors')
-const helmet = require('helmet')
+const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config();
 
 const app = express();
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(helmet())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
 
 app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' })
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
   }
-  next()
-})
+  next();
+});
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganSetting));
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
-app.listen(8000, () => {
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
   console.log("server running on 8000");
 });
 app.get("/testpoint", (req, res) => {
